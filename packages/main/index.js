84b138ae2a6cd027313aaca4c36a5b7ee06f2839
gusalbukrk <gusalbukrk@gmail.com>
2022-01-25 20:26:55 -0300

    initial commit

---
+++ packages/main/index.js
@@ -0,0 +1,4 @@
+import { add, mul } from 'math';
+
+console.log(add(7, 3));
+console.log(mul(7, 3));
---
+++ packages/math/index.js
@@ -0,0 +1,3 @@
+/* My add function. */
+export const add = (x, y) => x + y;
+export const mul = (x, y) => x * y;
